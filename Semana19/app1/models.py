e851fd29eca2e0e92027ceba9800dcea4a3179a7
Ricardo1703-svg <ralvares961@gmail.com>
2023-11-24 09:39:57 -0600

    Django

--- Semana19/app1/models.py
+++ Semana19/app1/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Proveedores(models.Model):
+    nombre=models.CharField(max_length=100)
+    telefono=models.CharField(max_length=8)
+
+    def __str__(self):
+        return self.nombre
+
+class Productos(models.Model):
+    nombre = models.CharField(max_length=100)
+    stock = models.PositiveIntegerField()
+    fk_prov=models.ForeignKey(Proveedores,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
