c471542903de5ae5fa5c94b89e71fa6e7d20d28e
Ricardo1703-svg <ralvares961@gmail.com>
2023-11-25 18:53:30 -0600

    Usuarios

--- Semana19/app1/views.py
+++ Semana19/app1/views.py
@@ -6,18 +6,17 @@ from django.http import HttpResponseRedirect
 from .models import Productos,Proveedores
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
-
+ 
 def reg_user(request):
     if request.method == "POST":
         formulario = NewUserForm(request.POST)
         if formulario.is_valid():
             formulario.save()
-        return HttpResponseRedirect("/")
+            return HttpResponseRedirect("/")
     else:
         formulario = NewUserForm()
-        render(request,"Reg_user.html",{"form":formulario})  
-        return
-    
+        return render(request, "Reg_user.html", {"form": formulario})
+
 def index(request):
     return render(request, 'index.html')
 
