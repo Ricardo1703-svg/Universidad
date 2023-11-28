93eec6a8cbde18b2cb6495b078fcd9ed1a8adcd9
Ricardo1703-svg <ralvares961@gmail.com>
2023-11-28 09:05:47 -0600

    Se agrego PySerial

--- Arduino.py
+++ Arduino.py
@@ -1,22 +1,24 @@
-import serial
 import tkinter as tk
 from tkinter import messagebox
 from pymongo import MongoClient
 from time import strftime
+import serial
 
-ser = serial.Serial('COM3', 9600)
-# Conexion
-client = MongoClient("mongodb://root12023:root2023@localhost:27017/?authMechanism=DEFAULT&authSource=admin")
+# Inicializar la conexión serial
+ser = serial.Serial('COM5', 9600)
+
+# Conexión a la base de datos MongoDB
+client = MongoClient("mongodb://Ricardo:root2023@localhost:27017/?authMechanism=DEFAULT")
 db = client["Arduino"]
 collection = db["TemperaturayHumedad"]
 
-# Funcion para Recibir e imprimir los datos desde el arduino
+# Función para recibir e imprimir los datos desde Arduino
 def actualizar_etiqueta():
     data = ser.readline().decode('utf-8').rstrip()
     etiqueta.config(text="Dato recibido: " + data)
     root.after(1000, actualizar_etiqueta)
 
-# Funcion para imprimir la fecha y hora en tiempo real
+# Función para imprimir la fecha y hora en tiempo real
 def actualizar_hora():
     string_hora = strftime('%H:%M:%S %p')
     etiqueta_hora.config(text=string_hora)
@@ -24,43 +26,52 @@ def actualizar_hora():
     etiqueta_fecha.config(text=string_fecha)
     root.after(1000, actualizar_hora)
 
-
-def Guardar_Datos():
-    Datos_Arduino = ser.readline().decode('utf-8').rstrip()
-    Datos_Hora = strftime('%H:%M:%S %p')
-    Datos_Fecha = strftime('%d/%m/%Y')
-    documento = {"Datos": Datos_Arduino, "Fecha": Datos_Fecha,"Hora": Datos_Hora}
+# Función para guardar datos en la base de datos
+def guardar_datos():
+    datos_arduino = ser.readline().decode('utf-8').rstrip()
+    datos_hora = strftime('%H:%M:%S %p')
+    datos_fecha = strftime('%d/%m/%Y')
+    documento = {"Datos": datos_arduino, "Fecha": datos_fecha, "Hora": datos_hora}
     result = collection.insert_one(documento)
     messagebox.showinfo("Genial", f"Documento insertado con id: {result.inserted_id}!")
 
-# Diseño de Ventana
+# Diseño de la ventana
 root = tk.Tk()
 root.title("Datos desde Arduino")
-root.geometry("800x600")
+
+# Centrar la ventana en la pantalla
+window_width = 800
+window_height = 600
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x_coordinate = (screen_width/2) - (window_width/2)
+y_coordinate = (screen_height/2) - (window_height/2)
+root.geometry(f"{window_width}x{window_height}+{int(x_coordinate)}+{int(y_coordinate)}")
 
 # Elementos de la GUI
-#-----------------------------------------Hora-----------------------------------------------------
 etiqueta_hora = tk.Label(root, font=('calibri', 10, 'bold'), background='black', foreground='white')
 etiqueta_hora.pack(anchor='center')
-#-----------------------------------------Fecha------------------------------------------------------
+
 etiqueta_fecha = tk.Label(root, font=('calibri', 10, 'bold'), background='red', foreground='black')
 etiqueta_fecha.pack(anchor='center')
-#----------------------------------------------Arduino-------------------------------------------------------------------
-etiqueta = tk.Label(root, text="Esperando datos...",font=('calibri', 10, 'bold'), background='black', foreground='white')
+
+etiqueta = tk.Label(root, text="Esperando datos...", font=('calibri', 10, 'bold'), background='black', foreground='white')
 etiqueta.pack(pady=10)
 
-# Acuaizacion de los datos
+# Actualización de los datos
 actualizar_etiqueta()
 actualizar_hora()
 
+# Botón para guardar datos
+insertar_button = tk.Button(root, text="Guardar", command=guardar_datos, bg="#8bff82", font=("Arial Black", 11))
+insertar_button.place(x=10, y=100)
+
+# Cerrar la conexión serial al cerrar la ventana
 def cerrar_ventana():
     ser.close()
     root.destroy()
+
 root.protocol("WM_DELETE_WINDOW", cerrar_ventana)
-
-insertar_button = tk.Button(root, text="Guardar", command=Guardar_Datos, bg="#8bff82",font = ("Arial Black", 11))
-insertar_button.place(x=10, y=100)
-
 
 # Iniciar el bucle principal de Tkinter
 root.mainloop()
