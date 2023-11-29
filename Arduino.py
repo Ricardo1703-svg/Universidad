4e280b102c384abe726750baf20865e89e834a93
Ricardo1703-svg <ralvares961@gmail.com>
2023-11-29 09:39:56 -0600

    Se agrego fpdf

--- Arduino.py
+++ Arduino.py
@@ -2,23 +2,20 @@ import tkinter as tk
 from tkinter import messagebox
 from pymongo import MongoClient
 from time import strftime
+from fpdf import FPDF  # Añade esta importación para trabajar con PDF
 import serial
 
-# Inicializar la conexión serial
 ser = serial.Serial('COM5', 9600)
 
-# Conexión a la base de datos MongoDB
 client = MongoClient("mongodb://Ricardo:root2023@localhost:27017/?authMechanism=DEFAULT")
 db = client["Arduino"]
 collection = db["TemperaturayHumedad"]
 
-# Función para recibir e imprimir los datos desde Arduino
 def actualizar_etiqueta():
     data = ser.readline().decode('utf-8').rstrip()
     etiqueta.config(text="Dato recibido: " + data)
     root.after(1000, actualizar_etiqueta)
 
-# Función para imprimir la fecha y hora en tiempo real
 def actualizar_hora():
     string_hora = strftime('%H:%M:%S %p')
     etiqueta_hora.config(text=string_hora)
@@ -26,7 +23,6 @@ def actualizar_hora():
     etiqueta_fecha.config(text=string_fecha)
     root.after(1000, actualizar_hora)
 
-# Función para guardar datos en la base de datos
 def guardar_datos():
     datos_arduino = ser.readline().decode('utf-8').rstrip()
     datos_hora = strftime('%H:%M:%S %p')
@@ -35,32 +31,38 @@ def guardar_datos():
     result = collection.insert_one(documento)
     messagebox.showinfo("Genial", f"Documento insertado con id: {result.inserted_id}!")
 
-# Diseño de la ventana
+    # Mostrar resultados en una nueva ventana
+    mostrar_resultados(datos_arduino, datos_hora, datos_fecha)
+
+    # Generar un PDF con los resultados
+    generar_pdf(datos_arduino, datos_hora, datos_fecha)
+
+def mostrar_resultados(datos_arduino, datos_hora, datos_fecha):
+    resultados_window = tk.Toplevel(root)
+    resultados_window.title("Resultados")
+    
+    tk.Label(resultados_window, text=f"Datos Arduino: {datos_arduino}").pack()
+    tk.Label(resultados_window, text=f"Hora: {datos_hora}").pack()
+    tk.Label(resultados_window, text=f"Fecha: {datos_fecha}").pack()
+
+def generar_pdf(datos_arduino, datos_hora, datos_fecha):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.cell(200, 10, txt="Resultados", ln=True, align='C')
+    pdf.ln(10)
+    pdf.cell(200, 10, txt=f"Datos Arduino: {datos_arduino}", ln=True)
+    pdf.cell(200, 10, txt=f"Hora: {datos_hora}", ln=True)
+    pdf.cell(200, 10, txt=f"Fecha: {datos_fecha}", ln=True)
+
+    pdf_output_path = "resultados.pdf"
+    pdf.output(pdf_output_path)
+    messagebox.showinfo("Genial", f"PDF generado con éxito: {pdf_output_path}")
+
 root = tk.Tk()
 root.title("Datos desde Arduino")
 
-# Centrar la ventana en la pantalla
-window_width = 800
-window_height = 600
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-x_coordinate = (screen_width/2) - (window_width/2)
-y_coordinate = (screen_height/2) - (window_height/2)
-root.geometry(f"{window_width}x{window_height}+{int(x_coordinate)}+{int(y_coordinate)}")
-
-# Elementos de la GUI
-etiqueta_hora = tk.Label(root, font=('calibri', 10, 'bold'), background='black', foreground='white')
-etiqueta_hora.pack(anchor='center')
-
-etiqueta_fecha = tk.Label(root, font=('calibri', 10, 'bold'), background='red', foreground='black')
-etiqueta_fecha.pack(anchor='center')
-
-etiqueta = tk.Label(root, text="Esperando datos...", font=('calibri', 10, 'bold'), background='black', foreground='white')
-etiqueta.pack(pady=10)
-
-# Actualización de los datos
-actualizar_etiqueta()
-actualizar_hora()
+# Resto del código...
 
 # Botón para guardar datos
 insertar_button = tk.Button(root, text="Guardar", command=guardar_datos, bg="#8bff82", font=("Arial Black", 11))
